fac74b2eee658cc8a3f9aabf20cc899cc2fc7393
Aman <aman01428@gmail.com>
2024-01-03 23:48:31 +0530

    Added Meals Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React,{ Fragment } from 'react';
-import './App.css';
-import Header from './Components/Layout/Header';
+// import './App.css';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 }
 
 export default App;
+
+
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,14 +1,17 @@
 import { Fragment } from "react";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
+
       <header className={classes.header}>
-        <h1>React Meals</h1>
-        <button>Cart</button>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
       </header>
+
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="img"/>
       </div>
