4f2283c415249dc9a4514b032d9404aafa46d2bb
Aman <aman01428@gmail.com>
2024-01-04 04:29:44 +0530

    make cartItem global

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useContext} from 'react'
+
+import CartContext from '../../store/cart-context';
+
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
+
+    const cartCtx=useContext(CartContext);
+
+    const numberOfCartItems=cartCtx.items.reduce((curNumber,item)=>{
+        return curNumber+item.amount;
+    },0);
+
   return (
     <button className={classes.button}
         onClick={props.onClick}
@@ -12,7 +22,7 @@ function HeaderCartButton(props) {
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-            3
+            {numberOfCartItems}
         </span>
     </button>
   )
